05a746c07b4cc742d4011c0d27d802c844b71edb
Szymon Kwiatkowski <szymon.z.kwiatkowski@gmail.com>
2024-06-03 21:57:03 +0200

    feat: additional processing for new model

--- person_segmentation_ros/person_segmentation_ros.py
+++ person_segmentation_ros/person_segmentation_ros.py
@@ -30,8 +30,8 @@ class PersonSegmentationRos:
         self.dtype = dtype
         self._uint16_max = 2**16 - 1
         self._detection_state = False
-
-        self._min_camera_range = 0.7 # min range which is 0.0
+        self._threshold = .65
+        self._min_camera_range = .7 # min range which is 0.0
         self._max_camera_range = 12 # max range which would be equal to uint16 max
 
         self.detection_distance = np.clip(
@@ -49,7 +49,7 @@ class PersonSegmentationRos:
         frame = np.asarray(cv2.resize(frame, (self.input_width, self.input_height)))
         input_data = self.preprocess(frame, dtype=self.dtype)
         outputs = self.session.run([self.output_name], {self.input_name: input_data})[0]
-        predicted_mask = np.where(outputs < 0.5, 0, 255).astype(np.uint8)
+        predicted_mask = np.where(outputs < self._threshold , 0, 255).astype(np.uint8)
         return np.squeeze(predicted_mask)
     
     def sendMqttMessage(self, message: bool):
@@ -57,20 +57,22 @@ class PersonSegmentationRos:
     
     def processReceivedFrames(self, image: np.ndarray, stereo_image: np.ndarray) -> bool:
        segmentation_mask = self.infer(image)
-       segmentation_mask = cv2.erode(segmentation_mask, np.ones((5, 5)), iterations=5)
-       segmentation_mask = cv2.dilate(segmentation_mask, np.ones((3, 5)), iterations=5)
+       segmentation_mask = cv2.erode(segmentation_mask, np.ones((3, 3)), iterations=4)
+      #  segmentation_mask = cv2.dilate(segmentation_mask, np.ones((5, 5)), iterations=5)
        image = cv2.resize(image, (self.input_width, self.input_height))
        stereo_image = np.array(cv2.resize(stereo_image, (self.input_width, self.input_height)), dtype=np.uint16)
        processed_img = self.stereoSegmentedFusion(mask=segmentation_mask, stereo=stereo_image)
        # Save results in local directory
-       cv2.imwrite('mask_applied.jpg', processed_img)
+       cv2.imwrite('mask_applied.jpg', np.array(processed_img / 256, dtype=np.uint8))
        cv2.imwrite('mask.jpg', segmentation_mask)
        cv2.imwrite('image.jpg', image)
-       cv2.imwrite('stereo.jpg', stereo_image)
+       cv2.imwrite('stereo.jpg', np.array(stereo_image / 256, dtype=np.uint8))
        return self.detectionAction(processed_img)
 
     def stereoSegmentedFusion(self, mask: np.ndarray, stereo: np.ndarray) -> np.ndarray:
+       stereo_cp = stereo.copy()
        stereo[mask<=0]=self._uint16_max
+       stereo = cv2.add(stereo_cp, stereo)
        return np.array(stereo, dtype=np.uint16)
     
     def detectionAction(self, detection_img: np.ndarray) -> bool:
